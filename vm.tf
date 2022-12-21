02986231c24e25f63392a4f76c5236bc8e1ecba4
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-21 17:45:05 -0500

    delegate postgres to subnet

--- vm.tf
+++ vm.tf
@@ -10,6 +10,16 @@ resource "azurerm_subnet" "default" {
   resource_group_name  = azurerm_resource_group.takahe-pindropt-fail.name
   virtual_network_name = azurerm_virtual_network.takahe.name
   address_prefixes     = ["10.0.0.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
 }
 
 resource "azurerm_public_ip" "ip" {
