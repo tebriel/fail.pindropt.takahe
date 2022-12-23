d8c8d05f9ea66ca3fc5134add8ac5b50bf1687e2
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-22 21:50:22 -0500

    remove network rules

--- storage.tf
+++ storage.tf
@@ -1,13 +1,13 @@
 resource "azurerm_storage_account" "media" {
   name                = "takahepindroptfail"
   resource_group_name = azurerm_resource_group.takahe-pindropt-fail.name
+  location            = azurerm_resource_group.takahe-pindropt-fail.location
 
-  location                 = azurerm_resource_group.takahe-pindropt-fail.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  network_rules {
-    default_action             = "Allow"
-    virtual_network_subnet_ids = [azurerm_subnet.default.id]
-  }
+#   network_rules {
+#     default_action             = "Allow"
+#     virtual_network_subnet_ids = [azurerm_subnet.default.id]
+#   }
 }
