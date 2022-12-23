b3b8567a9be23747500130860373a6ab425ea36a
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-22 21:57:37 -0500

    Fix name

--- storage.tf
+++ storage.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "media" {
   account_replication_type = "LRS"
 }
 resource "azurerm_storage_account_network_rules" "default" {
-  storage_account_id = azurerm_storage_account.example.id
+  storage_account_id = azurerm_storage_account.media.id
 
   default_action             = "Allow"
   ip_rules                   = [azurerm_public_ip.ip.ip_address]
