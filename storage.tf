5fcea166e856e179fc8f07107f8bad811dcaf600
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-22 22:09:01 -0500

    more flags

--- storage.tf
+++ storage.tf
@@ -3,9 +3,12 @@ resource "azurerm_storage_account" "media" {
   resource_group_name = azurerm_resource_group.takahe-pindropt-fail.name
   location            = azurerm_resource_group.takahe-pindropt-fail.location
 
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  nfsv3_enabled            = true
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  enable_https_traffic_only = false
+  account_replication_type  = "LRS"
+  nfsv3_enabled             = true
+  is_hns_enabled            = true
 }
 resource "azurerm_storage_account_network_rules" "default" {
   storage_account_id = azurerm_storage_account.media.id
